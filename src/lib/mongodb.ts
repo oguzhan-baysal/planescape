2034f2478b3ff4939d28adbd089c5e5e495b6d63
Oguzhan <oguzhanbaysal@outlook.com>
2024-09-24 16:38:32 +0300

    npm run build

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -9,7 +9,6 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
@@ -18,13 +17,13 @@ if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
